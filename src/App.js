1fcd23f44a7413290238b70df7a0c9863a348799
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-10-19 15:54:18 +0800

    styling the mainpage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// TODO: This will be have a lot of posts
+// TODO: Don't forget the header
+
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app">
+			<div className="app__header">
+				<img
+					src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+					className="app__headerImage"
+					alt=""
+				/>
+			</div>
+			<h1>Instagram Clone</h1>
+		</div>
+	);
 }
 
 export default App;
